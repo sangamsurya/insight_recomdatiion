3964ccf7eeab1b153e6242b5b89e431ee8eb54bd
SuryaTech <surya.sangam@techaffy.com>
2025-09-16 15:45:45 +0530

    new_port

--- Backend/app.py
+++ Backend/app.py
@@ -94,4 +94,5 @@ def serve_static(path):
     return send_from_directory(app.static_folder, path)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
